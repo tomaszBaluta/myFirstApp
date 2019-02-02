59d68e06363e8dfe43eb75c37ef48a78ceb6a09b
tomaszBaluta <t.baluta@gmail.com>
2019-02-02 15:58:58 +0100

    This is my first commit! Be aware!

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://galleria.riza.it/files/thumbs/640x316c/2018-08/lenticchie-ricetta.jpg'),
+    new Recipe('A test recipe2', 'This is a test2', 'https://galleria.riza.it/files/thumbs/640x316c/2018-08/lenticchie-ricetta.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shooping-list/shooping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredent} from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shooping-list',
+  templateUrl: './shooping-list.component.html',
+  styleUrls: ['./shooping-list.component.css']
+})
+export class ShoopingListComponent implements OnInit {
+  ingredients: Ingredent[] = [
+    new Ingredent('Apples', 5),
+    new Ingredent('Tomatoes', 10),
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
